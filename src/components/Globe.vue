43d0715a5069df385b316ed6ae16ffd317929e92
Jesse L.K. Overton <jesse@ovrtn.com>
2023-03-07 20:03:34 -0500

    Add moons

--- src/components/Globe.vue
+++ src/components/Globe.vue
@@ -15,25 +15,66 @@ function getFullUrl(relativeUrl) {
 }
 
 onMounted(() => {
-  const Auberean = Globe({animateIn: false });
+  const Auberean = Globe({ animateIn: false });
+
+
+  const moon1Texture = new THREE.TextureLoader().load(getFullUrl("/auberean-globe/albarel-rect.jpg"));
+  const moon2Texture = new THREE.TextureLoader().load(getFullUrl("/auberean-globe/rezarel-rect.jpg"))
+
+
+  const moon1 = {
+        lat: 25,
+    lng: 200,
+    alt: 0.8,
+    radius: 16,
+    color: 'orange',
+    texture: moon1Texture
+
+  };
+  const moon2 = {
+    lat: 20,
+    lng: 180,
+    alt: 0.6,
+    radius: 15,
+    color: 'red',
+    texture: moon2Texture
+  };
+  const N = 2;
+
+  const gData = [moon1, moon2];
 
   console.log(getFullUrl("/auberean-globe/auberean.jpg"));
   console.log(aubereanRegions);
-  Auberean(globeEl.value).globeImageUrl(getFullUrl("/auberean-globe/auberean-upscale.jpg"));
+  Auberean(globeEl.value).globeImageUrl(
+    getFullUrl("/auberean-globe/auberean-upscale.jpg")
+  );
 
   Auberean.backgroundImageUrl(
     "//unpkg.com/three-globe/example/img/night-sky.png"
   );
 
-//   Auberean.bumpImageUrl(
-//     getFullUrl("/auberean.png")
-//   );
+  //   Auberean.bumpImageUrl(
+  //     getFullUrl("/auberean.png")
+  //   );
+
+  Auberean.customLayerData(gData)
+    .customThreeObject(
+      (d) =>
+        new THREE.Mesh(
+          new THREE.SphereGeometry(d.radius),
+          new THREE.MeshBasicMaterial( { map: d.texture, overdraw: 0.5 } )
+        //   new THREE.MeshLambertMaterial({ color: d.color }),
+          
+        )
+    )
+    .customThreeObjectUpdate((obj, d) => {
+      Object.assign(obj.position, Auberean.getCoords(d.lat, d.lng, d.alt));
+    });
 
   Auberean.atmosphereAltitude(0.18);
 
-
-    Auberean.controls().autoRotate = true;
-    Auberean.controls().autoRotateSpeed = 1.5;
+  Auberean.controls().autoRotate = true;
+  Auberean.controls().autoRotateSpeed = 1.5;
 
   const globeMaterial = Auberean.globeMaterial();
   globeMaterial.bumpScale = 20;
